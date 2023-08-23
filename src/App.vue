735251f101c74dac2b285a9cee10ce10298aec76
Sh1n3zZ <DengJunHai@foxmail.com>
2023-08-23 13:13:29 +0800

    ✨ [Change] 添加了百度統計、添加了版權聲明
     ✨ [Change] 添加了百度統計、添加了版權聲明

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,6 @@ if (storage.focusInput) window.addEventListener('load', () => {
 });
 </script>
 
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d40a6fc111c20e7283541c35a065fecf";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
-
 <template>
   <main>
     <Background :focus="focus" />
